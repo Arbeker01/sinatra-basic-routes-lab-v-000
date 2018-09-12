9c9b9c907b515886f831848c98b78acca9ba4c9e
Romeo Gono <romeogono1@aol.com>
2018-09-12 01:35:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+
+  get '/name' do
+    "My name is ___"
+  end
+
+  get '/hometown' do
+    "My hometown is ___"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ___"
+  end
 end
